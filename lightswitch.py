e689f418010e5a90635f1b2a897f8c4f49ec7a51
Jelle0Bol <99067766@mydavinci.nl>
2022-01-19 11:07:31 +0100

    complete lichtschakelaar

--- lightswitch.py
+++ lightswitch.py
@@ -1,13 +1,29 @@
 import tkinter as tk
 window = tk.Tk()
 
-button = tk.Button(text='...', bg="white", fg="black")
-button.pack(pady = 20, padx = 20)
-
-# schijf hier tussen je code
-
 
 
 # schijf hier tussen je code
 
+global is_on
+is_on = True
+window.configure(bg='yellow')
+
+
+def switch():
+    global is_on
+    if is_on:
+        print("lamp is uit")
+        window.configure(bg='black')
+        button.configure(text="uit")
+        is_on = False
+    else:
+        print("lamp is aan")
+        window.configure(bg='yellow')
+        button.configure(text="aan")
+        is_on = True
+
+# schijf hier tussen je code
+button = tk.Button(text="aan", bg="white", fg="black", command=switch,pady = 50, padx = 100,)
+button.pack(pady = 300, padx = 600,)
 window.mainloop()
